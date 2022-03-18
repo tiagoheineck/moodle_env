43077015f7d31db89b8564f3f16fde3363e79c0e
Tiago Heineck <tiago.heineck@enap.gov.br>
2022-03-18 15:42:55 -0300

    adiciona arquivos

---
+++ .docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+if ! [ -f "/var/www/html/moodle/index.php" ]; then
+    echo "[ ****** ] Clonando o moodle na versão ${MOODLE_VERSION}"    
+    git clone -b "MOODLE_${MOODLE_VERSION}_STABLE" git://git.moodle.org/moodle.git /var/www/html/moodle
+fi
+
+chown www-data:www-data /var/www/html/moodledata
+chown www-data:www-data /var/www/html/moodle
+
+
+exec "$@"
---
+++ README.md
@@ -0,0 +1,15 @@
+# Rode o script que vai baixar a branch do moodle e criar a imagem dos containers
+
+```
+./install.sh
+```
+
+# Execute os containers
+
+```
+docker-compose up -d
+```
+
+# Siga as instruções de instalação do moodle no navegador
+
+URL: http://localhost:8000
